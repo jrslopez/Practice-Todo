23728698f30007a487baa39fc426520431ff28b0
James Lopez <james@jamess-mbp.mynetworksettings.com>
2023-09-20 00:26:36 -0400

    added put and delete

--- server/index.js
+++ server/index.js
@@ -52,7 +52,30 @@ app.get("/todos/:id", async (req,res) => {
 
 //update a todo
 
+app.put("/todos/:id", async (req,res) => {
+    try {
+       const {id} = req.params
+       const {description} = req.body
+       const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id])
+       
+       res.json("Todo was updated")
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
 //delete a todo
+
+app.delete("/todos/:id", async (req,res) => {
+    try {
+        const {id} = req.params
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id])
+        
+        res.json("deleted!")
+    } catch (error) {
+        console.error(error.message)
+    }
+})
 
 app.listen(3000, () => {
     console.log("server has started on port 3000")
