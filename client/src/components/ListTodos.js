fa77ec900da7dca09af7663c7684f3ae307dbd27
James Lopez <james@jamess-mbp.mynetworksettings.com>
2023-09-21 23:46:27 -0400

    added delete

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -2,6 +2,21 @@ import React, { Fragment, useState, useEffect } from "react"
 
 const ListTodos = () => {
   const [todos, setTodos] = useState([])
+
+  //delete todo
+
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:4000/todos/${id}`, {
+        method: "DELETE",
+      })
+
+      setTodos(todos.filter((todo) => todo.todo_id !== id))
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
   const getTodos = async () => {
     try {
       const response = await fetch("http://localhost:4000/todos")
@@ -28,10 +43,17 @@ const ListTodos = () => {
         </thead>
         <tbody>
           {todos.map((todo) => (
-            <tr>
+            <tr key={todo.todo_id}>
               <td>{todo.description}</td>
               <td>Edit</td>
-              <td>Delete</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
