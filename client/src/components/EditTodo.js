f5f92d1fd862d5a2392e9ac50cf576cceeece0be
James Lopez <james@jamess-mbp.mynetworksettings.com>
2023-09-22 23:03:07 -0400

    implemented edit function

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -1,48 +1,90 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useState } from "react"
 
-const EditTodo = () => {
+const EditTodo = ({ todo }) => {
+  const [description, setDescription] = useState(todo.description)
+
+  const updateDescription = async (e) => {
+    e.preventDefault()
+    try {
+      const body = { description }
+      const response = await fetch(
+        `http://localhost:4000/todos/${todo.todo_id}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body),
+        }
+      )
+      window.location = "/"
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
   return (
     <Fragment>
       <button
         type="button"
-        class="btn btn-primary"
+        class="btn btn-warning"
         data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
+        data-bs-target={`#id${todo.todo_id}`}
       >
         Edit
       </button>
 
       <div
         class="modal fade"
-        id="exampleModal"
+        id={`id${todo.todo_id}`}
         tabindex="-1"
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
+        onClick={(e) => {
+          setDescription(todo.description)
+        }}
       >
         <div class="modal-dialog modal-dialog-centered">
           <div class="modal-content">
             <div class="modal-header">
               <h1 class="modal-title fs-5" id="exampleModalLabel">
-                Modal title
+                Edit Todo
               </h1>
               <button
                 type="button"
                 class="btn-close"
                 data-bs-dismiss="modal"
                 aria-label="Close"
+                onClick={() => {
+                  setDescription(todo.description)
+                }}
               ></button>
             </div>
-            <div class="modal-body">Hello World</div>
+            <div class="modal-body">
+              <input
+                type="text"
+                className="form-control"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              ></input>
+            </div>
             <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-warning"
+                data-bs-dismiss="modal"
+                onClick={(e) => {
+                  updateDescription(e)
+                }}
+              >
+                Edit
+              </button>
               <button
                 type="button"
                 class="btn btn-secondary"
                 data-bs-dismiss="modal"
+                onClick={() => {
+                  setDescription(todo.description)
+                }}
               >
                 Close
-              </button>
-              <button type="button" class="btn btn-primary">
-                Save changes
               </button>
             </div>
           </div>
--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -47,7 +47,7 @@ const ListTodos = () => {
             <tr key={todo.todo_id}>
               <td>{todo.description}</td>
               <td>
-                <EditTodo />
+                <EditTodo todo={todo} />
               </td>
               <td>
                 <button
